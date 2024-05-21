7ea2d6de052d9fb08875df09b5670aeb545a90d7
adhim <adhimxtkj@gmail.com>
2024-05-22 02:01:16 +0800

    update: routing vue

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,6 @@ import HomeView from '../views/Home.vue'
 import LoginComponent from '../components/Login.vue'
 import RegisterComponent from '../components/Register.vue'
 import RuasJalanComponent from '../components/CreateRuasJalan.vue'
-import Uji from '../components/ujipolyline.vue'
 import DataRuasJalanComponent from '../components/DataRuasJalan.vue'
 
 const router = createRouter({
@@ -23,11 +22,6 @@ const router = createRouter({
       path: '/register',
       name: 'Register',
       component: RegisterComponent
-    },
-    {
-      path: '/uji',
-      name: 'Register',
-      component: Uji
     },
     {
       path: '/dataruasjalan',
